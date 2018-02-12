58ba3e163f002b8a3ee8da2ffb53395d5885b9d5
Chazareix Arnault <arnault.chazareix@student.ecp.fr>
2018-02-12 01:49:33 +0100

    base converter example

--- src/main/scala/IVF/Sample.scala
+++ src/main/scala/IVF/Sample.scala
@@ -15,104 +15,70 @@ object Sample {
   val astSamples: Map[String, SampleAST] =
     List[SampleAST](
       SampleAST(
-        "course",
-        Sequence(List(
+        "base_converter",
+        If(
+          Comparator(Less, Variable("X"), AValue(0)),
+          Skip(),
           If(
-            Comparator(LessEqual, Variable("X"), AValue(0)),
-            Assign(Variable("X"), AUnary(Sub, Variable("X"))),
-            Assign(Variable("X"), ABinary(Sub, AValue(1), Variable("X")))
-          ),
-          If(
-            Comparator(Equal, Variable("X"), AValue(1)),
-            Assign(Variable("X"), AValue(1)),
-            Assign(Variable("X"), ABinary(Plus, AValue(1), Variable("X")))
-          ),
-        )),
+            Comparator(LessEqual, Variable("base"), AValue(0)),
+            Skip(),
+            Sequence(List(
+              Assign(Variable("result"), AValue(0)),
+              Assign(Variable("factor"), AValue(1)),
+              While(
+                Comparator(GreaterEqual, Variable("X"), Variable("base")),
+                Sequence(List(
+                  Assign(Variable("rest"),
+                    ABinary(Sub,
+                      Variable("X"),
+                      ABinary(Mul,
+                        ABinary(Div,
+                          Variable("X"),
+                          Variable("base")
+                        ),
+                        Variable("base")
+                      )
+                    )
+                  ),
+                  Assign(Variable("result"),
+                    ABinary(Plus,
+                      Variable("result"),
+                      ABinary(Mul,
+                        Variable("rest"),
+                        Variable("factor")
+                      )
+                    )
+                  ),
+                  Assign(Variable("X"),
+                    ABinary(Div, Variable("X"), Variable("base"))
+                  ),
+                  Assign(Variable("factor"),
+                    ABinary(Mul, Variable("factor"), AValue(10))
+                  )
+                ))
+              )
+            )
+            )
+          )
+        ),
         List(
           SampleTest(
             AllAssignCriterion(),
             List(
-              List[State](State(Map("X" -> 1)), State(Map("X" -> -1)), State(Map("X" -> -2))),
-              List[State](State(Map("X" -> -1)), State(Map("X" -> -2)))
-            )
-          ),
-          SampleTest(
-            AllDecisionsCriterion(),
-            List(
-              List[State](State(Map("X" -> 1)), State(Map("X" -> -1)), State(Map("X" -> -2))),
-              List[State](State(Map("X" -> -1)), State(Map("X" -> -2)))
-            )
-          ),
-          SampleTest(
-            AllKPathCriterion(4),
-            List(
-              List[State](State(Map("X" -> 1)), State(Map("X" -> -1)), State(Map("X" -> -2))),
+              List(State(Map("X" -> -1, "base" -> -1)), State(Map("X" -> 2, "base" -> -1))),
+              List(State(Map("X" -> 0, "base" -> 2))),
+              List(State(Map("X" -> 4, "base" -> 2)))
             )
           ),
           SampleTest(
             AllDefinitionsCriterion(),
             List(
-              List[State](State(Map("X" -> 1)), State(Map("X" -> -1)), State(Map("X" -> -2))),
-            )
-          ),
-          SampleTest(
-            AllUsagesCriterion(),
-            List(
-              List[State](State(Map("X" -> 1))),
-              List[State](State(Map("X" -> -2))),
-            )
-          ),
-          SampleTest(
-            AllDUPathsCriterion(),
-            List(
-              List[State](State(Map("X" -> 1))),
-              List[State](State(Map("X" -> -2))),
-              List[State](State(Map("X" -> 1)), State(Map("X" -> -2))),
+              List(State(Map("X" -> -1, "base" -> -1)), State(Map("X" -> 2, "base" -> -1))),
+              List(State(Map("X" -> 0, "base" -> 2))),
+              List(State(Map("X" -> 4, "base" -> 2)))
             )
           )
         )
-      ),
-      SampleAST(
-        "k_path", Sequence(List(
-          Skip(),
-          While(Comparator(GreaterEqual, Variable("X"), AValue(2)),
-            If(Comparator(GreaterEqual, Variable("Y"), AValue(2)),
-              Sequence(
-                List(
-                  Assign(Variable("Y"), ABinary(Sub, Variable("Y"), AValue(1))),
-                  Assign(Variable("X"), ABinary(Sub, Variable("X"), AValue(1)))
-                )
-              ),
-              Assign(Variable("X"), ABinary(Sub, Variable("X"), AValue(1)))
-            )
-          )
-        )),
-        List(
-          SampleTest(
-            AllKPathCriterion(9),
-            List(
-              List(State(Map("X" -> 2, "Y" -> -1)), State(Map("X" -> 2, "Y" -> 2)), State(Map("X" -> 3, "Y" -> 2)), State(Map("X" -> -1)), State(Map("X" -> 3, "Y" -> -1)))
-            )
-          ),
-          SampleTest(
-            AllUsagesCriterion(3),
-            List(
-              List(State(Map("X" -> 2, "Y" -> -1)), State(Map("X" -> 2, "Y" -> 2)), State(Map("X" -> 3, "Y" -> 2)), State(Map("X" -> -1)), State(Map("X" -> 3, "Y" -> -1))),
-              List(State(Map("X" -> 5, "Y" -> 2)), State(Map("X" -> 5, "Y" -> 1)), State(Map("X" -> 6, "Y" -> 6)), State(Map("X" -> 4, "Y" -> 4)), State(Map("X" -> 5, "Y" -> 5)), State(Map("X" -> 4, "Y" -> 3)), State(Map("X" -> 6, "Y" -> 2)))
-            )
-          )
-        )
-      ),
-      SampleAST(
-        "usage_vs_du", Sequence(List(
-          Assign(Variable("Y"), Variable("X")),
-          If(
-            BValue(false),
-            Skip(),
-            Skip()
-          ),
-          Assign(Variable("Z"), Variable("Y")),
-        ))
       )
     )
       .map(s => s.name -> s).toMap
